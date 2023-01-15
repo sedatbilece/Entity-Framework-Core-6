33e618f6c792f44298f4cf7e045bbc186db6246b
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2023-01-15 13:25:01 +0300

    İnitialize CodeFirst Project

---
+++ EFCORE6.CodeFirst/DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCORE6.CodeFirst.DAL
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+        public AppDbContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=MATEBOOK-SEDAT\\SQLEXPRESS;initial Catalog=EFCORE6CodeFirstDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ EFCORE6.CodeFirst/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFCORE6.CodeFirst.DAL;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World! from codefirst");
+
+
+
+using (var _context = new AppDbContext())
+{
+    var producsts = await _context.Products.ToListAsync();
+
+    foreach (var item in producsts)
+    {
+        Console.WriteLine($"{item.Id} : {item.Name} : {item.Price}  {item.Stock}");
+    }
+}
