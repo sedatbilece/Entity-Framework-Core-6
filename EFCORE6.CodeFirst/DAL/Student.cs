f92c016b1276c53c5bb7ab31be0f53c702e47015
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2023-01-17 18:34:48 +0300

    add Student,Teacher and ManytoMany relation

---
+++ EFCORE6.CodeFirst/DAL/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCORE6.CodeFirst.DAL
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public int Age { get; set; }
+
+        public List<Teacher> Teachers { get; set; }
+    }
+}
---
+++ EFCORE6.CodeFirst/Migrations/20230117153048_manytomany.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCORE6.CodeFirst.Migrations
+{
+    public partial class manytomany : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
