97620a9c03027555d7e4b1f7425bdd6569ab6755
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2023-01-27 12:13:27 +0300

    add mvc structure and Concurrency using RowVersion

---
+++ Concurrency.Web/Models/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Concurrency.Web.Models
+{
+    public class AppDbContext:DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().Property(x => x.RowVersion).IsRowVersion();
+            modelBuilder.Entity<Product>().Property(x => x.Price).HasPrecision(18, 2);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
